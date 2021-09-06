02064b72db3bdc35513d4401b6811e748c9aad04
Ahmed Roble <airoble2019@gmail.com>
2021-09-06 12:31:31 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react"
+import Loading from "./Loading"
+import Tour from "./Tour"
+import Tours from "./Tours"
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project"
+
+function App() {
+  return (
+    <main>
+      <Tours />
+    </main>
+  )
+}
+
+export default App
